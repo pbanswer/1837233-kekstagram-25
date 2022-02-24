3792e8b045e767b85fc3e51aa157fcaf59bccdb1
pbanswer <pbanswer@yandex.ru>
2022-02-24 23:59:01 +0300

    исправил по списку

--- js/main.js
+++ js/main.js
@@ -1,36 +1,23 @@
-"use strict";
-// взято с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// случайное число из диапазона
+// код взят с https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// и немного изменен
 // логи для моего понимания))
 
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
   if ((min < max) && (min >= 0)) {
-    let randomNumber = Math.random();
-    let randomNumberFloor = Math.floor(Math.random())
-    let result = Math.floor(randomNumber * (max - min + 1)) + min;
-    console.log(randomNumberFloor);
-    console.log(randomNumber);
+    const randomNumber = Math.random();
+    const result = Math.floor(randomNumber * (max - min + 1)) + min;
     return result;
   }
-  else {
-    console.log('Максимальное значение диапазона меньше минимального или минимальное значение диапазона меньше нуля')
-  }
-}
+};
 
-let printRnd = getRandomIntInclusive(100,150)
-console.log(printRnd)
+const printRnd = getRandomIntInclusive(100,150);
+printRnd();
 
 
 // проверка длины строки
 
-let stringLength = function (string, maxLength) {
-  if (string.length > maxLength) {
-    console.log('Превышает')
-    return false;
-  } else {
-    console.log('Прокатило')
-    return true;
-  }
-}
+const checkStrLength = (string, maxLength) => string.length < maxLength;
 
-let printLng = stringLength('Это строка длиной 29 символов', 20)
-console.log(printLng)
+const printLng = checkStrLength('Эта строка длиной 29 символов', 20);
+printLng();

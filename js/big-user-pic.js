3ce0ee8f87798063f04741fa3f94f8ff45429970
pbanswer <pbanswer@yandex.ru>
2022-04-05 00:25:38 +0300

    модуль 8 - 1,2 задание

--- js/big-user-pic.js
+++ js/big-user-pic.js
@@ -3,9 +3,14 @@ const bigPicImg = bigUserPic.querySelector('.big-picture__img');
 const bigPicLikes = bigUserPic.querySelector('.likes-count');
 const bigPicComments = bigUserPic.querySelector('.comments-count');
 const commentsList = bigUserPic.querySelector('.social__comments');
+const commentsCount = bigUserPic.querySelector('.social__comment-count');
+const moreCommentsButton = document.querySelector('.comments-loader');
+
+const KEYTAP = 'Escape';
 
 
 const openBigPic = (picture) => {
+  let lastComment = 5;
 
   bigUserPic.classList.remove('hidden');
 
@@ -17,24 +22,53 @@ const openBigPic = (picture) => {
   const commentFragment = document.createDocumentFragment();
   commentsList.innerHTML = '';
 
-  picture.comments.forEach((comment) => {
+  const template = document.querySelector('#comment-template').content;
+  const templateElement = template.querySelector('.social__comment');
 
-    const template = document.querySelector('#comment-template').content;
-    const templateElement = template.querySelector('.social__comment');
-    const templateCopy = templateElement.cloneNode(true);
+  if (picture.comments.length <= 5) {
+    picture.comments.forEach((comment) => {
+      const templateCopy = templateElement.cloneNode(true);
+      lastComment = picture.comments.length;
 
-    templateCopy.querySelector('.social__picture').src = comment.avatar;
-    templateCopy.querySelector('.social__picture').alt = comment.name;
-    templateCopy.querySelector('.social__text').textContent = comment.message;
-    commentFragment.appendChild(templateCopy);
-    //console.log(commentFragment.appendChild(templateCopy));
+      templateCopy.querySelector('.social__picture').src = comment.avatar;
+      templateCopy.querySelector('.social__picture').alt = comment.name;
+      templateCopy.querySelector('.social__text').textContent = comment.message;
+      commentFragment.appendChild(templateCopy);
+      bigUserPic.querySelector('.comments-loader').classList.add('hidden');
+      //console.log(commentFragment.appendChild(templateCopy));
+    });
+  } else {
+    for (let i = 0; i < lastComment; i++) {
+      const templateCopy = templateElement.cloneNode(true);
+
+      const comment = picture.comments;
+      //console.log(comment[i]);
+      templateCopy.querySelector('.social__picture').src = comment[i].avatar;
+      templateCopy.querySelector('.social__picture').alt = comment[i].name;
+      templateCopy.querySelector('.social__text').textContent = comment[i].message;
+      commentFragment.appendChild(templateCopy);
+      bigUserPic.querySelector('.comments-loader').classList.remove('hidden');
+    }
+  }
+
+  commentsCount.textContent = `${ lastComment } из ${ bigPicComments.textContent }`;
+
+  moreCommentsButton.addEventListener('click', () => {
+    lastComment =+ 5;
+    //console.log('click');
   });
 
   commentsList.appendChild(commentFragment);
 
   bigUserPic.querySelector('.social__caption').textContent = picture.description;
-  bigUserPic.querySelector('.social__comment-count').classList.add('hidden');
-  bigUserPic.querySelector('.comments-loader').classList.add('hidden');
+  //bigUserPic.querySelector('.social__comment-count').classList.add('hidden');
+  //bigUserPic.querySelector('.comments-loader').classList.add('hidden');
+
+  if (commentsList.children.length > 5) {
+    bigUserPic.querySelector('.social__comment-count').classList.remove('hidden');
+    bigUserPic.querySelector('.comments-loader').classList.remove('hidden');
+  }
+  //console.log(`здесь выводится кол-во комментариев${  commentsList.children.length}`);
   document.body.classList.add('modal-open');
 
   const closeButton = document.querySelector('.big-picture__cancel');
@@ -45,9 +79,8 @@ const openBigPic = (picture) => {
     window.removeEventListener('keydown', onEscKeydown);
   };
 
-  const keyTap = 'Escape';
   function onEscKeydown (evt) {
-    if (evt.key === keyTap) {
+    if (evt.key === KEYTAP) {
       bigUserPic.classList.add('hidden');
       closeButton.removeEventListener('click', onCloseClick);
       window.removeEventListener('keydown', onEscKeydown);
--- js/form.js
+++ js/form.js
@@ -1,14 +1,9 @@
 // валидация формы
 
 const form = document.querySelector('.img-upload__form');
-//new Pristine(form);
+const hashtags = form.querySelector('.text__hashtags');
 
-/* const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/
-console.log(re.test('#11')); */
-
-
-
-const pristine = new Pristine( form, {
+const pristine = new window.Pristine( form, {
   classTo: 'img-upload__text',
   errorClass: 'img-upload__text--invalid',
   successClass: 'img-upload__text--valid',
@@ -17,62 +12,60 @@ const pristine = new Pristine( form, {
   errorTextClass: 'form__error'
 });
 
-function validateHTag (value) {
-  return value.length >= 2 && value.length <= 20;
-}
-
-pristine.addValidator(
-  form.querySelector('.text__hashtags'),
-  validateHTag,
-  'От 2 до 20 символов'
-);
 
 //проверка на повтор хэштега
 const checkDuplicate = (tags) => {
-  let duplicates = []
-  const tempArray = tags.sort()
+  const duplicates = [];
+  const tempArray = tags.sort();
   for (let i = 0; i < tempArray.length; i++) {
     if (tempArray[i + 1] === tempArray[i]) {
-      duplicates.push(tempArray[i])
+      duplicates.push(tempArray[i]);
     }
   }
-  console.log(duplicates)
-  if (duplicates.length > 1) {
-    console.log(duplicates.length);
-    return false
+  //console.log(duplicates);
+  if (duplicates.length >= 1) {
+    //console.log(`здесь значение дубликатов: ${  duplicates.length}`);
+    return false;
   } else {
-    return true
+    return true;
   }
-}
+};
 
 // проверка хэштега
 const checkHTag = () => {
-  const text = document.querySelector(".text__hashtags");
-  const val = text.value;
+  const tags = form.querySelector('.text__hashtags');
+  const val = tags.value;
+  //console.log(val);
   const arr = val.split(' ');
-  console.log(val);
-  console.log(arr);
-  checkDuplicate(arr);
-  console.log('это вывод checkDup: ' +  checkDuplicate(arr));
-  if (arr.length > 5) {
-    console.log('Не более 5ти тегов')
-  } else {
-      arr.forEach(element => {
-        const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/i
-        console.log(re.test(element))
+  //console.log(val);
+  //console.log(`это массив:  ${ arr}`);
+  const checkResult = checkDuplicate(arr);
+  //console.log(`это вывод checkDup: ${   checkResult}`);
+  if (checkResult === true) {
+    if (arr.length > 5) {
+      //console.log('Не более 5ти тегов');
+      return false;
+    } else {
+      arr.forEach((element) => {
+        const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/i;
+        //console.log(re.test(element));
+        if ( re.test(element)) {
+          return true;
+        } else {
+          return false;
+        }
       });
     }
-}
+  } else {
+    return false;
+  }
+};
 
-
-//pristine.addValidator(form.querySelector('.text__hashtags'));
-
+pristine.addValidator(hashtags, checkHTag, 'Хэштег состоит из букв и чисел,  от 2 до 20 символов, начинается с решётки, максимум 5 хэштегов, без повторений', 1 , true);
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  checkHTag();
   pristine.validate();
-
 });
 
 
@@ -86,11 +79,11 @@ const documentBody = document.querySelector('body');
 inputUploadFile.addEventListener('change', () => {
   imageEditForm.classList.remove('hidden');
   documentBody.classList.add('modal-open');
-})
+});
 
 
 formButtonCancel.addEventListener('click', () => {
   imageEditForm.classList.add('hidden');
   documentBody.classList.remove('modal-open');
-  console.log('клик');
-})
+  //console.log('клик');
+});
